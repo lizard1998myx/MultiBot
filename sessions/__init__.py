80b3ebb5cdfa8277b94c3c921acd8df7f2993cf2
Sgt.Lizard <lizard1998myx@gmail.com>
2022-04-08 17:43:41 +0800

    Delete sessions directory

--- sessions/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-用于保存各类插件
-基本的Session类存放于general.py中
-一个功能更丰富的ArgSession类存放于argument.py中
-各个Session完工于不同时期，使用的包以及代码风格都有差异
-完成一个新的Session后需要手动添加到主目录下的session_initialize.py中，否则不会生效
-同样地，你可以在session_initialize.py中禁用一些不需要的session
-"""
